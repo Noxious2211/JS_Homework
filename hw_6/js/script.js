58570e8e607c32165004b1e8672a0fc89ad57e2d
Dmitry <Noxious22@mail.ru>
2020-06-28 19:27:53 +0300

    #

--- hw_6/js/script.js
+++ hw_6/js/script.js
@@ -1,9 +1,9 @@
 document.querySelector('.header__mobile-ico').onclick = function () {
    document.querySelector('.mobile-ico').classList.toggle('mobile-ico-active');
    document.querySelector('.header__mobile-menu').classList.toggle('header__mobile-menu-active');
-}
+};
 document.querySelector('.fa-search').onclick = function () {
    document.querySelector('.header__input').classList.toggle('header__input-active');
    document.querySelector('.header_user').classList.toggle('header__user-active');
-}
+};
 
