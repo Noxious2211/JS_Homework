9b5fd7d811752f2b96597957135d968370a70021
Dmitry <Noxious22@mail.ru>
2020-06-29 11:09:36 +0300

    N_4

---
+++ hw_7/script.js
@@ -0,0 +1,45 @@
+
+function Animal(name) {
+   var foodAmount = 50;
+
+   function formatFoodAmount() {
+      return foodAmount + ' гр.';
+   }
+
+   this.dalyNorm = function (amount) {
+      if (!arguments.length) return formatFoodAmount();
+      if (amount < 50 || amount > 500) {
+         return 'not';
+      }
+      foodAmount = amount;
+   };
+   this.name = name;
+
+   this.feed = function () {
+      console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма');
+      // console.log('кот доволен ^_^');
+   };
+}
+
+var barsik = new Animal('Барсик');
+console.log(barsik.name);
+
+console.log(barsik.dalyNorm(250));
+console.log(barsik.feed());
+
+
+function Cat(name) {
+   Animal.call(this);
+
+   this.name = name;
+
+   var animalFeed = this.feed;
+   this.feed = function () {
+      animalFeed();
+      console.log('кот доволен ^_^');
+
+   }
+}
+var cats = new Cat('tom');
+console.log(cats.name);
+console.log(cats.feed());
